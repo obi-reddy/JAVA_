0aaca4643e19b4395f10e6b0e0a00e805b50c159
KANCHI REDDY GARI OBI REDDY <93593081+obi-reddy@users.noreply.github.com>
2023-05-11 05:32:51 +0200

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+# Data Types and Uses
+
+Java supports a wide range of data types, which can be used to store and manipulate different kinds of data.
+  Some of the most common data types in Java include:
+
+1. Primitive Data Types: These are the basic data types in Java, and they include
+boolean, byte, short, int, long, float, and double. These data types are used to store simple values like numbers and boolean values.
+
+2. Object Data Types: Java also supports object data types, which include String, Arrays, and 
+other user-defined classes. These data types are used to store more complex data, such as text strings, collections of data, and custom data structures.
+
+Data types are useful in Java because they provide a way to represent and manipulate data in a consistent and structured way.
+  This makes it easier to write, read, and maintain Java programs, especially when working with large and complex data sets.
+  Additionally, the use of data types can help improve the performance of Java programs, by allowing the JVM to allocate memory more efficiently and by 
+  reducing the overhead of type conversion during runtime. Overall, understanding the different data types in Java is an important part of becoming 
+  proficient in the language, and can help you write more effective and efficient code.
